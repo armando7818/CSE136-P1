d986048794b9cd28355460e22597d141c3b38639
Yuanchao Zhu <zyc262626@gmail.com>
2016-04-11 16:40:32 -0700

    Env variable sort

--- python/env.py
+++ python/env.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import cgi, cgitb
 import os
+from operator import itemgetter
 
 cgitb.enable()
 print 'Content-Type: text/html;charset=utf-8'
@@ -17,8 +18,9 @@ print '</head>'
 print '<body>'
 print '<h1>Environment Variables</h1>'
 env = os.environ
-print '<table style="width:100%">'
-for k, v in env.items():
+print '<table style="width:80%">'
+env_sorted = sorted(env.items(), key = itemgetter(0))
+for k, v in env_sorted:
     print '<tr>'
 
     print '<td>'
